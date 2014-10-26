3cb00a20cf4d402cb307949220295b8ec5f2c86f
Cellule <mike.ferris@hotmail.com>
2014-10-25 21:06:17 -0400

    Basic option parser

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 /// <reference path="typings/tsd.d.ts" />
-var modules = require("./modules.json");
+var program = require('commander');
+
+program.version('0.0.1').option('-c, --config [file]', 'Choose config file with module list [modules.json]', 'modules.json').parse(process.argv);
+
+var path = require("path");
+var configPath = path.resolve(program.config);
+var modules = require(configPath);
 console.log(modules);
